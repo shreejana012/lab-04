b0f933a2843d0c3df49991d4a3704de4c6271b51
Shrijana Poudel <spoude14@my.centennialcollege.ca>
2025-03-24 17:55:24 -0400

    added string extensions class

---
+++ lab-04/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace lab_04;
+
+public static class StringExtensions
+{
+    public static bool StartsWithUpper(this string str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+
+        char firstChar = str[0];
+        return char.IsUpper(firstChar);
+    }
+}
